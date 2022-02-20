fe04e49781f1e28e77e953b40084d0d977c6845f
James Rogers <jimrogers3315@gmail.com>
2022-02-20 05:36:52 -0600

    Create Person,Address controller

---
+++ MVCMiniProject/Controllers/AddressController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCMiniProject.Controllers
+{
+    public class AddressController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AddressController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AddressController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
